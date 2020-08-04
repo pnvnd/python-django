b8e2ce6a694a43277c018547dad15eb22769bb76
pnvnd <peter@datacrunch.ca>
2020-08-04 00:29:01 -0400

    Fixed DEBUG setting

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -134,7 +134,7 @@ LOGIN_URL = 'users:login'
 import django_heroku
 django_heroku.settings(locals())
 
-if os.environ.get('DEBUG_VALUE') == 'TRUE':
+if os.environ.get('DEBUG') == 'TRUE':
     DEBUG = True
-elif os.environ.get('DEBUG_VALUE') == 'FALSE':
+elif os.environ.get('DEBUG') == 'FALSE':
     DEBUG = False
