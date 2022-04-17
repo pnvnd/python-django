5559fdbd04e19d514a162ce6f90e744f0df5a655
pnvnd <peter@datacrunch.ca>
2022-04-17 19:40:09 -0400

    Updated settings to work on localhost

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u8&o=tuwmu%9lq=&!!4wxncyxdr3q$*&xs5hpjtxvy+v4%#n*+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
